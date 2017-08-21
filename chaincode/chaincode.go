c44fb48f4d3bfb82bff8bd27d37c76aabf656119
kvukic-croz <31210284+kvukic-croz@users.noreply.github.com>
2017-08-21 16:46:23 +0200

    Update chaincode.go

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 // SimpleChaincode example simple Chaincode implementation
